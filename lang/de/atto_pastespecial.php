5352ee1e45d00db0545f49176b7619996dbce32f
Bernhard Weichel <github.com@nospam.weichel21.de>
2016-02-19 16:58:39 +0100

    #12 add German translation
    updated the German wording as requested by jinhofer

--- lang/de/atto_pastespecial.php
+++ lang/de/atto_pastespecial.php
@@ -23,15 +23,17 @@
  */
 
 $string['pluginname'] = 'Paste special';
-$string['pastehere'] = 'Inhalt hier einfügen';
+$string['pastehere'] = '1. einzufügender Inhalt';
 $string['pastefromword'] = 'Einfügen aus Microsoft Word';
 $string['pastefromgdoc'] = 'Einfügen aus Google Document';
 $string['pastefromlibre'] = 'Einfügen aus LibreWriter';
 $string['pastefromother'] = 'Einfügen aus anderer Anwendung';
+$string['pastefrommoodle'] = 'Einfügen aus Moodle';
+$string['pastestraight'] = 'Einfügen als Text ohne HTML zu bereinigen   ';
 $string['pasteunformatted'] = 'Einfügen als unformatierten Text';
-$string['paste'] = 'Einfügen';
+$string['paste'] = 'Einfügen abschließe (Ctrl-Shift-Enter)';
 $string['settings'] = 'Einstellungen für Paste special';
-$string['wordCSS_desc'] = 'Erlaubte CSS-Eigenschaften beim Einfügen aus Microsoft Word (Format siehe Standard)';
+$string['wordCSS_desc'] = 'Erlaubte CSS-Eigenschaften beimEinfügen aus Microsoft Word (Format siehe Standard)';
 $string['gdocCSS_desc'] = 'Erlaubte CSS-Eigenschaften beim Einfügen aus Google Document (Format siehe Standard)';
 $string['libreCSS_desc'] = 'Erlaubte CSS-Eigenschaften beim Einfügen aus LibreWriter (Format siehe Stan dard)';
 $string['otherCSS_desc'] ='Erlaubte CSS-Eigenschaften beim Einfügen aus anderer Anwendung (Format siehe Standard)';
@@ -44,4 +46,15 @@ $string['gdocCSS_default'] = 'background-color,color,font-family,font-size,font-
 $string['libreCSS_default'] = 'background,color,font-size';
 $string['default'] = '"Paste sppecial" standardmässig aktiviert';
 $string['default_desc'] = 'Wenn aktiviert, öffnet der Tastaturbefehl (ctrl-v / cmd-v) "Paste special" anstatt den Inhalt der Zwischenablage direkt einzufügen.';
-$string['pasteview'] = 'Vorschau:';
+$string['pasteview'] = '3. Vorschau:';
+$string['straight'] = 'Direkt einfügen';
+$string['straight_desc'] = 'Erlaubt es Text einzufügen ohne HTMl zu bereinigen';
+$string['help'] = 'Hilfe';
+$string['help_text'] = 'Folgen Sie den aufgezählten Schritten um Text in ATTO einzufügen. Dieses Plugin bereinigt vermülltes HTML aus Ihrer Quelle, und fügt eine bereingte Version in Atto ein. Wenn Sie ihre Formatierung (aus ästhetischen Gründen) beibehalten wollen, wählnen sie den Radiobutton entprechendd der Quelle aus der Sie den formatierten Text kopiert haben. Wenn Sie keine Formatierung beibehalten wollen, wählen Sie den Radiobutton "Einfügen als unformatiertn Text". Um diese Hilfe auszubelden klicken sie bitte ' . $string['help'] .'" erneut.';
+$string['cancel'] = 'Abbrechen';
+$string['clickthebutton'] = '4. Klicken  Sie den Button: "'.$string['paste'].'!';
+$string['height'] = 'Höhe';
+$string['height_desc'] = 'Setzt die Höhe des Popup-Fensters in %';
+$string['width'] = 'Breite';
+$string['width_desc'] = 'Setzt die Breite des Popup-Fensters in %';
+$string['step2'] = '2. Wählen Sie die Quelle des Textes';
